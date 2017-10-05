febb214b75cd22eedeab8ec79abb2da6eb8a81de
CarlosFr97 <cmiguel1997@gmail.com>
2017-10-05 16:14:23 +0100

    fixed

--- LAIGDemoParser/main.js
+++ LAIGDemoParser/main.js
@@ -13,7 +13,7 @@ function getUrlVars() {
 
 serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js',
 			 'MyGraphNode.js', 'MyGraphLeaf.js', 'MyInterface.js',
-       'MyQuad.js','MySphere.js','MyTriangle.js','MyCylinder.js',
+       'primitives/MyQuad.js','primitives/MySphere.js','primitives/MyTriangle.js','primitives/MyCylinder.js',
 
 main=function()
 {
