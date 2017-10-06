fdbbdba2b1b1bc37125c9698be1ee413a232b63d
CarlosFr97 <cmiguel1997@gmail.com>
2017-10-06 14:12:18 +0100

    fixed quad textures

--- LAIGDemoParser/primitives/MyQuad.js
+++ LAIGDemoParser/primitives/MyQuad.js
@@ -3,15 +3,15 @@
  * @param gl {WebGLRenderingContext}
  * @constructor
  */
-function MyQuad(scene, minX, minY, maxX, maxY) {
+function MyQuad(scene, x1, y1, x2, y2) {
 	CGFobject.call(this, scene);
 	this.lengths = 1;
 	this.lengtht = 1;
 
-	this.minX = minX;
-	this.maxX = maxX;
-	this.minY = minY;
-	this.maxY = maxY;
+	this.x1 = x1;
+	this.x2 = x2;
+	this.y1 = y1;
+	this.y2 = y2;
 	this.initBuffers();
 };
 
@@ -20,11 +20,14 @@ MyQuad.prototype.constructor = MyQuad;
 
 MyQuad.prototype.initBuffers = function () {
 	this.vertices = [
-		this.minX, this.maxY, 0,
-		this.maxX, this.maxY, 0,
-		this.minX, this.minY, 0,
-		this.maxX, this.minY, 0
+		this.x1, this.y2, 0,
+		this.x2, this.y2, 0,
+		this.x1, this.y1, 0,
+		this.x2, this.y1, 0
 	];
+
+	this.height = this.y1-this.y2;
+	this.width = this.x2-this.x1; 
 
 	this.indices = [
 		0, 1, 2,
@@ -42,14 +45,18 @@ MyQuad.prototype.initBuffers = function () {
 	]
 
 	this.texCoords = [
-		0,this.lengtht,
-		this.lengths,this.lengtht,
+		0,this.height/this.lengtht,
+		this.width/this.lengths,this.height/this.lengtht,
 		0,0, 
-		this.lengths,0 
+		this.width/this.lengths,0 
 		
 
 
 	]
+
+
+
+
 	this.primitiveType = this.scene.gl.TRIANGLES;
 	this.initGLBuffers();
 };
@@ -59,5 +66,6 @@ MyQuad.prototype.loadTexture = function(texture)
 {
 	this.lengths = texture[1];
 	this.lengtht = texture[2];
+	this.initBuffers();
 	
 }
