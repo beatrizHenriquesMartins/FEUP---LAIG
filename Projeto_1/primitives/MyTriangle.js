1fc259c24b4116f4b0d2624d5e5c475ada25b0f9
Carlos Miguel da Silva de Freitas <cmiguel1997@gmail.com>
2017-10-21 01:32:40 +0100

    clock added

--- Projeto_1/primitives/MyTriangle.js
+++ Projeto_1/primitives/MyTriangle.js
@@ -24,16 +24,17 @@ MyTriangle.prototype.constructor = MyTriangle;
 
 MyTriangle.prototype.initBuffers = function () {
 
-  this.a = Math.sqrt(Math.pow(this.x1-this.x3,2) + Math.pow(this.y1-this.y3,2) + Math.pow(this.z1-this.z3,2));
-  this.b = Math.sqrt(Math.pow(this.x2-this.x1,2) + Math.pow(this.y2-this.y1,2) + Math.pow(this.z2-this.z1,2));
-  this.c= Math.sqrt(Math.pow(this.x3-this.x2,2) + Math.pow(this.y3-this.y2,2) + Math.pow(this.z3-this.z2,2));
+  this.a = Math.sqrt(Math.pow(this.x1 - this.x3, 2) + Math.pow(this.y1 - this.y3, 2) + Math.pow(this.z1 - this.z3, 2));
+  this.b = Math.sqrt(Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2) + Math.pow(this.z2 - this.z1, 2));
+  this.c = Math.sqrt(Math.pow(this.x3 - this.x2, 2) + Math.pow(this.y3 - this.y2, 2) + Math.pow(this.z3 - this.z2, 2));
 
-  this.beta = (Math.pow(this.a,2) - Math.pow(this.b,2) + Math.pow(this.c,2)) / (2*this.a*this.c);
-  this.angle= Math.acos(this.beta);
+  this.beta = (Math.pow(this.a, 2) - Math.pow(this.b, 2) + Math.pow(this.c, 2)) / (2 * this.a * this.c);
+  this.angle = Math.acos(this.beta);
 
-  this.vertices = [ this.x1, this.y1, this.z1,
-                    this.x2, this.y2, this.z2,
-                    this.x3, this.y3, this.z3 ];
+  this.vertices = [this.x1, this.y1, this.z1,
+    this.x2, this.y2, this.z2,
+    this.x3, this.y3, this.z3
+  ];
 
   this.indices = [0, 1, 2];
 
@@ -47,25 +48,27 @@ MyTriangle.prototype.initBuffers = function () {
   this.normals.push(vecx, vecy, vecz);
   this.normals.push(vecx, vecy, vecz);
 
-  this.auxTexCoords = [ 0, this.lengtht,
-                        this.c/this.lengths, this.lengtht,
-                        this.c/this.lengths - (this.a/this.lengths * this.beta),
-                        this.lengtht-(this.a/this.lengtht * Math.sin(this.angle)) ];
+  this.auxTexCoords = [
 
-  this.texCoords = this.auxTexCoords.slice()
+    0, 1,
+    this.c, 1,
+    this.c - (this.a * this.beta),
+    1 - (this.a * Math.sin(this.angle))
+  ];
+
+  this.texCoords = this.auxTexCoords.slice();
   this.primitiveType = this.scene.gl.TRIANGLES;
   this.initGLBuffers();
 }
 
-MyTriangle.prototype.loadTexture = function(texture){
+MyTriangle.prototype.loadTexture = function (texture) {
 
   this.lengths = texture[1];
   this.lengtht = texture[2];
-  /*for(let i=0; i < this.auxTexCoords.length; i+=2)
-  {
-    this.texCoords[i] = this.auxTexCoords[i]/this.lengths;
-    this.texCoords[i+1] = this.auxTexCoords[i+1]/this.lengtht;
-  }*/
+  for (let i = 0; i < this.auxTexCoords.length; i += 2) {
+    this.texCoords[i] = this.auxTexCoords[i] / this.lengths;
+    this.texCoords[i + 1] = this.auxTexCoords[i + 1] / this.lengtht;
+  }
 
-  this.initBuffers();
+  this.updateTexCoordsGLBuffers()
 }
