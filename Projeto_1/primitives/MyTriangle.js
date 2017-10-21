0e00ed8d76e27f6299d50bf75889e687a9b07185
Carlos Miguel da Silva de Freitas <cmiguel1997@gmail.com>
2017-10-21 17:37:02 +0100

    texture triangle working

--- Projeto_1/primitives/MyTriangle.js
+++ Projeto_1/primitives/MyTriangle.js
@@ -23,13 +23,7 @@ MyTriangle.prototype = Object.create(CGFobject.prototype);
 MyTriangle.prototype.constructor = MyTriangle;
 
 MyTriangle.prototype.initBuffers = function () {
-
-  this.a = Math.sqrt(Math.pow(this.x1 - this.x3, 2) + Math.pow(this.y1 - this.y3, 2) + Math.pow(this.z1 - this.z3, 2));
-  this.b = Math.sqrt(Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2) + Math.pow(this.z2 - this.z1, 2));
-  this.c = Math.sqrt(Math.pow(this.x3 - this.x2, 2) + Math.pow(this.y3 - this.y2, 2) + Math.pow(this.z3 - this.z2, 2));
-
-  this.beta = (Math.pow(this.a, 2) - Math.pow(this.b, 2) + Math.pow(this.c, 2)) / (2 * this.a * this.c);
-  this.angle = Math.acos(this.beta);
+  
 
   this.vertices = [this.x1, this.y1, this.z1,
     this.x2, this.y2, this.z2,
@@ -48,7 +42,7 @@ MyTriangle.prototype.initBuffers = function () {
   this.normals.push(vecx, vecy, vecz);
   this.normals.push(vecx, vecy, vecz);
 
-  this.auxTexCoords = [
+  /*this.auxTexCoords = [
 
     0, 1,
     this.c, 1,
@@ -56,19 +50,47 @@ MyTriangle.prototype.initBuffers = function () {
     1 - (this.a * Math.sin(this.angle))
   ];
 
-  this.texCoords = this.auxTexCoords.slice();
+  this.texCoords = this.auxTexCoords.slice();*/
+
+  this.texCoords= [
+    0,1,
+    1,1,
+    1,0,
+  ]
   this.primitiveType = this.scene.gl.TRIANGLES;
   this.initGLBuffers();
 }
 
 MyTriangle.prototype.loadTexture = function (texture) {
 
-  this.lengths = texture[1];
-  this.lengtht = texture[2];
-  for (let i = 0; i < this.auxTexCoords.length; i += 2) {
+  var lengths = texture[1];
+  var lengtht = texture[2];
+
+  //a 1-2
+  //b 0-2
+  //c 0-1
+  var a = Math.sqrt(Math.pow(this.x2 - this.x3, 2) + Math.pow(this.y2 - this.y3, 2) + Math.pow(this.z2 - this.z3, 2));
+  var b = Math.sqrt(Math.pow(this.x1 - this.x3, 2) + Math.pow(this.y1 - this.y3, 2) + Math.pow(this.z1 - this.z3, 2));
+  var c = Math.sqrt(Math.pow(this.x1 - this.x2, 2) + Math.pow(this.y1 - this.y2, 2) + Math.pow(this.z1 - this.z2, 2));
+
+  var beta = (Math.pow(a, 2) - Math.pow(b, 2) + Math.pow(c, 2)) / (2 * a * c);
+  var angle = Math.acos(beta);
+
+  var h= a * Math.sin(angle);
+
+
+  /*for (let i = 0; i < this.auxTexCoords.length; i += 2) {
     this.texCoords[i] = this.auxTexCoords[i] / this.lengths;
     this.texCoords[i + 1] = this.auxTexCoords[i + 1] / this.lengtht;
-  }
+  }*/
+
+  this.texCoords = [
+    
+        0, h/lengtht,
+        c/lengths, h/lengtht,
+        (c - a * beta)/lengths,  (h - a * Math.sin(angle))/lengtht
+  ];
+
 
   this.updateTexCoordsGLBuffers()
 }
