9571bc0ad98af3cd17936fd2b1d28399a47ef6c8
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-11-16 10:21:00 +0000

    um pouco da classe linear animation

--- Projeto_2/Animation/LinearAnimation.js
+++ Projeto_2/Animation/LinearAnimation.js
@@ -1,14 +1,35 @@
 class LinearAnimation extends Animation {
-    constructor(scene, id,velocity, controlPoints) {
+    constructor(scene, id, velocity, controlPoints) {
         super(scene,id, velocity);
         this.p1 = controlPoints[0];
         this.p2 = controlPoints[1];
         this.p3 = controlPoints[2];
-    }
-    calcPoints(){
-        
+
+        if(controlPoints == NULL){
+          throw new Error("Sem pontos de controlo!");
+        }
+
+        this.distance = 0;
+
+        // para terminar a animacao
+        this.over = false;
     }
 
-    //x(t)=x0+v*t
-    //y(t)=y0+v*t
+    calcPoints(){
+      //x(t)=x0+v*t
+      //y(t)=y0+v*t
+      
+    }
+
+    update(){
+
+    }
+
+    display(){
+
+    }
+
+    clone(){
+      return new LinearAnimation(this.scene, this.id, this.velocity, this.controlPoints);
+    }
 }
