affb7024b8400c3e45ca39bfaf4cfbf44b561b66
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-11-16 10:47:14 +0000

    um pouco da circular animation

--- Projeto_2/Animation/CircularAnimation.js
+++ Projeto_2/Animation/CircularAnimation.js
@@ -5,9 +5,23 @@ class CicularAnimation extends Animation {
         this.ang_initial = ang_initial;
         this.rotation_angle = rotation_angle;
         this.center = center;
+
+        this.currentAngle = 0;
+        this.currentDistance = 0;
+
+        this.over = false;
     }
 
     calcPoints(){
-        
+      this.distance = this.rotation_angle * this.radius;
+    }
+
+    update(deltaTime){
+      var time = (deltaTime/1000) * this.velocity;
+    }
+
+    clone(){
+      return new CicularAnimation(this.scene, this.id,
+        this.velocity, this.radius, this.ang_initial, this.rotation_angle, this.center);
     }
 }
