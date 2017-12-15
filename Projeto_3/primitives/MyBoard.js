3fc358a5a3ca1d191f068ac632058c3869678fd8
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-12-15 16:31:52 +0000

    board corrigido

--- Projeto_3/primitives/MyBoard.js
+++ Projeto_3/primitives/MyBoard.js
@@ -1,4 +1,19 @@
 /**
+ * o---o---o---o---o---o
+ * |   |   |   |   |   |
+ * o---o---o---o---o---o
+ * |   |   |   |   |   |
+ * o---o---o---o---o---o
+ * |   |   |   |   |   |
+ * o---o---o---o---o---o
+ * |   |   |   |   |   |
+ * o---o---o---o---o---o
+ * |   |   |   |   |   |
+ * o---o---o---o---o---o
+ * need: circle, slip, cube
+ */
+
+ /**
  * MyBoard primitive
  * @constructor
  */
@@ -7,11 +22,27 @@ function MyBoard(scene, width, heigh, len) {
 
     console.log("board ");
   
+    //variaveis
     this.width = width;
     this.heigh = heigh;
     this.len = len;
 
+    // o tabuleiro é um quadrado, logo é OBRIGATÓTRIO a altura e largura serem iguais
+    if(this.width != this.heigh){
+        console.log("width and heigh must be equal");
+        return;
+    }
+
+    // material
+    this.materialMetal = new CGFappearance(this.scene);
+	this.materialMetal.setAmbient(0.3,0.3,0.3,1);
+	this.materialMetal.setDiffuse(0.25,0.25,0.25,1);
+	this.materialMetal.setSpecular(0.85,0.85,0.85,1);	
+    this.materialMetal.setShininess(580);
+    
+    // primitives
     this.cube = new MyUnitCubeQuad(scene);
+    this.strip =  new MyQuad(scene, 0.0, 0.5, 0.5, 0.0);
 
     this.initBuffers();
 };
@@ -19,30 +50,51 @@ function MyBoard(scene, width, heigh, len) {
 MyBoard.prototype = Object.create(CGFobject.prototype);
 MyBoard.prototype.constructor = MyBoard;
 
+/**
+ * draw one piece of de board
+ */
 MyBoard.prototype.drawQuad = function() {
     this.scene.pushMatrix();
         this.scene.scale(this.width, 1/3, this.heigh);
-        this.scene.translate((this.width/2), 0, 0);
+        this.scene.translate((this.width/2), 1/3, (this.heigh/2));
         this.cube.display();
     this.scene.popMatrix();    
 }
-  
+
+MyBoard.prototype.drawStrip_vertical = function() {
+    this.scene.pushMatrix();
+        this.scene.translate(-4, 0.3, -4);
+        this.scene.scale(0.5, 1/2, this.heigh*this.len);
+        this.scene.rotate(-(Math.PI / 2), 1, 0, 0);
+        this.scene.rotate(-(Math.PI / 2), 0, 0, 1);
+        this.materialMetal.apply();
+        this.strip.display();
+    this.scene.popMatrix();
+}
 /**
- * Initiates all WEBCGF atributes of the primitive
+ * draw de complete board
  */
 MyBoard.prototype.display = function () {
-    for(let j = 1; j <= this.len; j++){
-        if(j != 1){
-            this.scene.translate(-1 * this.len + this.width, 0, this.heigh);
-        }
-
-        for(let i = 1; i <= this.len; i++){
-            if(i === 1){
-                this.drawQuad();
-            }else{
-                this.scene.translate(this.width, 0, 0);
-                this.drawQuad();
+    //LINHA
+    for(let ind = 0; ind < this.len; ind++){
+        this.scene.pushMatrix();
+            if(ind != 0){
+                this.scene.translate(0, 0, (this.heigh * ind));
             }
-        }
+
+            //COLUNA
+            for (let index = 0; index < this.len; index++) {
+                if(index === 0){
+                    this.drawQuad();
+                }else{
+                    this.scene.pushMatrix();
+                        this.scene.translate((this.width * index), 0, 0);
+                        this.drawQuad();
+                    this.scene.popMatrix();
+                }
+            }
+        this.scene.popMatrix();
     }
+
+    this.drawStrip_vertical();
 };
