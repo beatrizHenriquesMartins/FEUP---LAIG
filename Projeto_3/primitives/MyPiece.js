34d942fcdcd196d7408a45ba8b3b378f50cddd98
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-12-27 00:26:37 +0000

    peça generica

--- Projeto_3/primitives/MyPiece.js
+++ Projeto_3/primitives/MyPiece.js
@@ -1,18 +1,28 @@
+/**
+ * MyPiece is constructor for all types of piece of the game
+ * @param {*} scene - scene 
+ * @param {*} type_piece - type of piece (white, black, mix)
+ */
 function MyPiece(scene, type_piece) {
     CGFobject.call(this, scene);
 
-    console.log("my piece");
+    console.log("entrou no constructor my piece");
+    console.log("type = ", type_piece);
 
     this.type_piece = type_piece;
 
-    console.log("depois de criar var");
-    
-    if(this.type_piece === "white" || this.type_piece === "black" || this.type_piece === "mix"){
-        console.log("the type of piece isn't wrong!");
+    if(this.type_piece === "white"){
+        console.log("Type of piece is the rigth type");
+    }else if(this.type_piece === "black"){
+        console.log("Type of piece is the rigth type");
+    }else if(this.type_piece === "mix"){
+        console.log("Type of piece is the rigth type");
+    }else{
+        console.log("error type");
         return;
     }
 
-    this.body = new MyCompleteCylinder(this.scene, 10, 10, 10, 10, 10);
+    this.body = new MyCompleteCylinder(this.scene, 1, 0.5, 0.5, 200, 200, 1, 1);
 
     this.initBuffers();
 };
@@ -22,10 +32,14 @@ MyPiece.prototype.constructor = MyPiece;
 
 MyPiece.prototype.draw_body = function(){
     this.scene.pushMatrix();
-        this.body.display();
+        this.scene.rotate(-(Math.PI)/2, 1, 0, 0);
+        this.body.display();        
     this.scene.popMatrix();
 }
 
+/**
+ * Initiates all Webcgf atributes of the primitive
+ */
 MyPiece.prototype.display = function () {
-  this.draw_body();
+    this.draw_body();
 };
