3c7b7cbcd85b2cb4eaa5a3cb2ae39c00bd196d8c
Beatriz de Henriques Martins <up201502858@fe.up.pt>
2017-12-27 11:54:58 +0000

    todas as peças desenhadas

--- Projeto_3/primitives/MyPiece.js
+++ Projeto_3/primitives/MyPiece.js
@@ -22,7 +22,29 @@ function MyPiece(scene, type_piece) {
         return;
     }
 
+    //EXEMPLE MATERIAL
+    this.material_1 = new CGFappearance(this.scene);
+    this.material_1.setAmbient(0.1, 0.1, 0.1, 1);
+    this.material_1.setDiffuse(0.1, 0.19, 0.3, 1);
+    this.material_1.setSpecular(0.5, 0.5, 0.5, 0);  
+    this.material_1.setShininess(110);
+
+    //white material
+    this.material_white = new CGFappearance(this.scene);
+    this.material_white.setAmbient(0.9, 0.95, 0.9, 1);
+    this.material_white.setDiffuse(0.99, 0.99, 0.99, 1);
+    this.material_white.setShininess(1, 1, 1, 1);
+    this.material_white.setShininess(120);
+
+    //black material
+    this.material_black = new CGFappearance(this.scene);
+    this.material_black.setAmbient(0, 0, 0, 1);
+    this.material_black.setDiffuse(0, 0, 0, 1);
+    this.material_black.setShininess(0, 0, 0, 1);
+    this.material_black.setShininess(120);
+
     this.body = new MyCompleteCylinder(this.scene, 1, 0.5, 0.5, 200, 200, 1, 1);
+    this.circle = new MyCircle(this.scene, 200, 0.25);
 
     this.initBuffers();
 };
@@ -30,6 +52,9 @@ function MyPiece(scene, type_piece) {
 MyPiece.prototype = Object.create(CGFobject.prototype);
 MyPiece.prototype.constructor = MyPiece;
 
+/**
+ * draw the body of piece
+ */
 MyPiece.prototype.draw_body = function(){
     this.scene.pushMatrix();
         this.scene.rotate(-(Math.PI)/2, 1, 0, 0);
@@ -38,8 +63,48 @@ MyPiece.prototype.draw_body = function(){
 }
 
 /**
+ * draw the circle of mix piece(white and black)
+ */
+MyPiece.prototype.draw_circle = function(){
+    this.scene.pushMatrix();
+        this.scene.translate(0, 1.001, 0);
+        this.scene.rotate(-(Math.PI)/2, 1, 0, 0);
+        this.circle.display();
+    this.scene.popMatrix();    
+};
+
+/**
+ * draw the mix piece, the body and the circle
+ */
+MyPiece.prototype.draw_mix_piece = function(){
+    this.scene.pushMatrix();
+        this.scene.pushMatrix();
+            this.material_black.apply();
+            this.draw_circle();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+            this.material_white.apply();
+            this.draw_body();
+        this.scene.popMatrix();
+    this.scene.popMatrix();
+}
+
+/**
  * Initiates all Webcgf atributes of the primitive
  */
 MyPiece.prototype.display = function () {
-    this.draw_body();
+    if (this.type_piece === "white") {
+        this.scene.pushMatrix();
+            this.material_white.apply();
+            this.draw_body();
+        this.scene.popMatrix();    
+    }else if (this.type_piece === "black") {
+        this.scene.pushMatrix();
+            this.material_black.apply();
+            this.draw_body();
+        this.scene.popMatrix();  
+    }else{
+        this.draw_mix_piece();
+    }
 };
